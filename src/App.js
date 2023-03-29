156de8fb570df71d383cf38d7e3e1a6dc32e812a
SuchithG <suchithgowda1995@gmail.com>
2023-03-29 17:50:21 +0530

    feat: :art: Added UseContext Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import UserFinder from './components/UserFinder';
-import Users from './components/Users';
+import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
+
+const DUMMY_USERS = [
+  { id: "U1", name: "Max" },
+  { id: "U2", name: "Manuel" },
+  { id: "U3", name: "Julie" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
